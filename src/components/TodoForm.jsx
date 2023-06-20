ba7524325eb06b0093d49db3f8cfbbbe8f58c2e8
Atiqul Hoque <73009994+atiqbaqi@users.noreply.github.com>
2023-06-20 19:35:34 +0600

    added some css and localstorage

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react"
+import './TodoForm.css'
 
 const  TodoForm = ({id}) => {
+    const storedTodo = JSON.parse(localStorage.getItem('todo'));
+    console.log(storedTodo);
     const [newTodo,setNewTodo] = useState('');
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(storedTodo);
 
     const addNewTodo = () => {
-        setTodoList(prevTodoList=>[...prevTodoList,newTodo]);
+        if(!newTodo) return;
+        const todo = {
+            title: newTodo,
+            is_done:0
+        };
+        let updatedTodoList = [];
+        // Retrieving the object from local storage
+        const prevTodoList = JSON.parse(localStorage.getItem('todo'));
+        if (Array.isArray(prevTodoList)) {
+            updatedTodoList = [...prevTodoList, todo];
+        }
+        localStorage.setItem('todo', JSON.stringify(updatedTodoList));
+        setTodoList(updatedTodoList);
         setNewTodo('');
         console.log(todoList);
     };
 
-    const removeTodo = (index) => {
+    const removeTodo = (index) => { 
         setTodoList((prevTodoList) => {
             const updatedList = [...prevTodoList];
             updatedList.splice(index, 1); // Remove the item at the specified index
+            localStorage.setItem('todo', JSON.stringify(updatedList));
             return updatedList;
           });
     };
@@ -21,22 +37,24 @@ const  TodoForm = ({id}) => {
     return(
         <>
             <div className="todo">
-                <label htmlFor={id}>New Item</label>
+                <label htmlFor={id} style={{fontSize:"20px",fontWeight:"bold"}}>New Item</label>
                 <br />
                 <input type="text" id={id} value={newTodo} onChange={e => setNewTodo(e.target.value)}/>
                 <br />
                 <button onClick={addNewTodo}>Add</button>
             </div>
+            <hr />
             <div className="todo-list">
-                <h1>ToDo List</h1>
+                <h4>ToDo List</h4>
                 <ul className="list">
-                {todoList.map((value, index) => (
+                {todoList.map((data, index) => (
                     <li key={index}>
                         <label>
                             <input type="checkbox"/>
-                            {value}
+                            {data.title}
                         </label>
-                        <button className="btn btn-danger" onClick={()=>removeTodo(index)}>Delete</button>
+                        <br />
+                        <a className="btn btn-danger" style={{cursor:'pointer',fontSize:'12px'}} onClick={()=>removeTodo(index)}>Delete</a>
                     </li>
                 ))}
                 </ul>
